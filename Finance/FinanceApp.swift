fa7084277857bb6fda62bbea9a5ca49f99bf928c
Gautam <gautam.menon01@gmail.com>
2023-08-26 13:05:10 +0530

    Initial Commit

---
+++ Finance/FinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceApp.swift
+//  Finance
+//
+//  Created by Gautam Menon on 26/08/23.
+//
+
+import SwiftUI
+
+@main
+struct FinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
